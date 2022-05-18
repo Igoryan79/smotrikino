458a71e0501d95479b2549e32265c147f6fe7255
igoryan79 <deloporabote12@yandex.ru>
2022-05-18 13:11:05 +0300

    добав. режиссеров

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const films = require('./data/films.json');
+const directors = require('./data/directors.json');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -11,9 +12,47 @@ app.get('/', (req, res) => {
   })
 });
 
+app.get('/films/:id', (req, res) => {
+    res.render('pages/movie-info', {
+    film: films.find((film)=>{
+      if (film.id===+req.params.id) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+  })
+});
+
+app.get('/directors', (req, res) => {
+  res.render('pages/directors', {
+    directors: directors
+  })
+});
+
+app.get('/directors/:id', (req, res) => {
+  res.render('pages/director', {
+  director: directors.find((director)=>{
+    if (director.id===+req.params.id) {
+      return true;
+    } else {
+      return false;
+    }
+  })
+})
+});
+
+app.get('/films', (req, res) => {  // моя часть кода
+  res.render('pages/movie-info', {
+    films: films
+  })
+});
+
 app.get('/contacts', (req, res) => {
     res.send('All contacts');
   });
+
+ 
 
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
